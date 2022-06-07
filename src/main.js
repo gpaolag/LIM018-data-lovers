3b72e4dcfdbe462a8ba12af0bad42d2b5cbf9a75
Paola <glenypaola.7@gmail.com>
2022-06-07 09:15:33 -0500

    Agregar el metodo de mostrar peliculas

--- src/main.js
+++ src/main.js
@@ -10,7 +10,21 @@ let navMenu = document.querySelector(".nav-menu");
     navToggle.addEventListener("click",()=>{
     navMenu.classList.toggle("nav-menu_visible");
     })
-
+const films = data.films;
+const mainmovies = document.querySelector("#carrucelPeliculas");
+    
+function showInfoMovies (arrayData){
+    mainmovies.innerHTML = "";
+    arrayData.forEach((filmsPublished) => {
+        const divFilm = document.createElement("div"); //div para cada tarjeta
+        divFilm.classList.add("div_content_movies");
+        divFilm.innerHTML=`<img src="${filmsPublished.poster}" class="div_img_movie" />
+        <b><h3 class="contenedor_section_h3__movti"><p class="films-titles">${filmsPublished.title}</h3></b>`;
+        divFilm.setAttribute("id", filmsPublished.id);
+        mainmovies.appendChild(divFilm);
+    });
+}
+showInfoMovies(films);
 /* const dataPeliculas = data.films;
 console.log(dataPeliculas); */
 
