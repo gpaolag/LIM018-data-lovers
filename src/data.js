05323c3df655a53519acfe04e85a6a1aeb871fcd
OryChRamirez <105568434+OryChRamirez@users.noreply.github.com>
2022-06-08 22:36:19 -0500

    se agregó css a la barra de busqueda / se agregó formula para que pueda filtrarse en la barra de busqueda (solo faltaría que filtre por personaje (array dentro de array)

--- src/data.js
+++ src/data.js
@@ -46,3 +46,13 @@ export function orderReciente(){
     showFilms = reciente;
     return showFilms;
 }
+
+export function search(){
+    let txtIngresado = document.querySelector('#inputBuscar').value;
+    let resultado = films.filter( x => 
+      `${x.title.toLocaleLowerCase()} ${x.director.toLocaleLowerCase()} ${x.producer.toLocaleLowerCase()} 
+      ${x.release_date.toLocaleLowerCase()} ${x.rt_score.toLocaleLowerCase()}`.includes(txtIngresado.toLowerCase()));
+    showFilms = resultado;
+    return showFilms;
+  }
+  
