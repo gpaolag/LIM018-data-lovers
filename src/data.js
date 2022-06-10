93227d7fbdff13cc80827406d1395f7106155c5b
OryChRamirez <105568434+OryChRamirez@users.noreply.github.com>
2022-06-09 20:13:11 -0500

    se agregó function newArrayPeople() como prueba para ubicar los personajes

--- src/data.js
+++ src/data.js
@@ -47,12 +47,20 @@ export function orderReciente(){
     return showFilms;
 }
 
+export function newArrayPeople(){
+    let newPeople = "";
+    for(let i=0; i<films.length; i++){
+        for(let j=0;j<films[i].people.length;j++){
+            newPeople = newPeople + films[i].people[j].name +",";
+        }}
+    newPeople = newPeople.split(",");
+    return newPeople;
+}
+
+
 export function search(){
-    let txtIngresado = document.querySelector('#inputBuscar').value;
-    let resultado = films.filter( x => 
-      `${x.title.toLocaleLowerCase()} ${x.director.toLocaleLowerCase()} ${x.producer.toLocaleLowerCase()} 
-      ${x.release_date.toLocaleLowerCase()} ${x.rt_score.toLocaleLowerCase()}`.includes(txtIngresado.toLowerCase()));
-    showFilms = resultado;
-    return showFilms;
-  }
-  
+  let resultado = films.filter( all => 
+    `${all.title.toLowerCase()} ${all.director.toLowerCase()} ${all.producer.toLowerCase()} ${all.release_date.toLowerCase()}`.includes(document.querySelector('#inputBuscar').value.toLowerCase()));
+  showFilms = resultado;
+  return showFilms;
+}
